e35784ff8d394eeb105614313dfabd49bea6785b
Manideepsai <nmanideepsai25@gmail.com>
2025-08-19 21:39:43 +0530

    Implementing frontend components in a modular and professional way

--- src/Landing_page/Products/Hero.js
+++ src/Landing_page/Products/Hero.js
@@ -1,15 +1,7 @@
 import React from 'react';
 
 function Hero() {
-    return ( 
-        <div className='container border-bottom mt-5'>
-            <div className='text-center mt-5 p-3'>
-           <h1>Technology</h1>
-           <h3 className='text-muted mt-3 fs-4'>Sleek, modren and intuitive trading platforms</h3>
-           <p className='mt-3'>Check out our <a href='' style={{textDecoration:"none"}}> investment offerings  <i className="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
-        </div>
-        </div>
-    );
+    return ( <h1>Hero</h1> );
 }
 
-export default Hero; 
+export default Hero;
---
+++ src/Landing_page/Support/GenTicket.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function GenTicket() {
+    return (<h1>GenTicket</h1>  );
+}
+
+export default GenTicket;
