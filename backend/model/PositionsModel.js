09006824553947561907ec65cb52157bd69956dd
Manideepsai <nmanideepsai25@gmail.com>
2025-11-20 11:18:49 +0530

    import in Positions

--- backend/model/PositionsModel.js
+++ backend/model/PositionsModel.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-const PositionSchema = require("../schemas/PositionsSchema");
-
-const PositionsModel = mongoose.model("positions", PositionSchema);
-
-module.exports = { PositionsModel };
-=======
 // model/PositionsModel.js
 import mongoose from "mongoose";
 import PositionSchema from "../schemas/PositionsSchema.js";
 
 export const PositionsModel = mongoose.model("Positions", PositionSchema);
->>>>>>> clean-main
