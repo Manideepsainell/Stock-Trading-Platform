7a2504b561d2448eab9d9d9cd73ff0f05cc1f93f
Manideepsai <nmanideepsai25@gmail.com>
2025-11-20 11:25:22 +0530

    ALL

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-const { model } = require("mongoose");
-const OrdersSchema = require("../schemas/OrdersSchema");  // no destructuring
-
-// ✅ register the model with the schema
-const OrdersModel = model("Orders", OrdersSchema); // use capitalized model name
-
-module.exports = { OrdersModel };
-=======
 // model/OrdersModel.js
 import { model } from "mongoose";
 import OrdersSchema from "../schemas/OrdersSchema.js";
 
 export const OrdersModel = model("Orders", OrdersSchema);
->>>>>>> clean-main
