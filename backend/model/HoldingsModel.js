6d743be5a83a88a572d796289d8afc6ab0009271
Manideepsai <nmanideepsai25@gmail.com>
2025-11-20 11:21:35 +0530

    import in Positions

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-const { model } = require("mongoose");
-const HoldingSchema = require("../schemas/HoldingSchema");  // no destructuring
-
-// ✅ register the model with the schema
-const HoldingsModel = model("Holding", HoldingSchema); // use capitalized model name
-
-module.exports = { HoldingsModel };
-=======
 // model/HoldingsModel.js
 import { model } from "mongoose";
 import HoldingSchema from "../schemas/HoldingSchema.js"; // default export from schema
 
 export const HoldingsModel = model("Holding", HoldingSchema);
->>>>>>> clean-main
